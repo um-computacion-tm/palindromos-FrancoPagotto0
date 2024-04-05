0fd892991a21d5de3b85fecc7c3d0f0914355b5b
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2024-04-05 11:02:12 -0300

    Elimine los tests del archivo palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,4 @@
-import unittest
+
 def is_palindrome(mystring):
     mystring = mystring.replace(" ","")
     for indice in range(0, len(mystring)):
@@ -8,37 +8,3 @@ def is_palindrome(mystring):
             return False
     return True
     
-
-class testpalindrome(unittest.TestCase):
-    def test_A (self):
-        resultado = is_palindrome("a")
-        self.assertEqual(resultado, True)
-
-    def test_B(self):
-        resultado = is_palindrome("aca")
-        self.assertEqual(resultado, True)   
-    
-    def test_C(self):
-        resultado = is_palindrome("ABCBA")
-        self.assertEqual(resultado, True)
-
-    def test_D(self):
-        resultado = is_palindrome("NEUQ UEN")
-        self.assertEqual(resultado, True)  
-
-    def test_E(self):
-        resultado = is_palindrome("ab")
-        self.assertEqual(resultado,False)
-
-    def test_F (self):
-        resultado = is_palindrome("hola")
-        self.assertEqual(resultado,False) 
-   
-    def test_G (self):
-        resultado = is_palindrome("Franco")
-        self.assertEqual(resultado,False) 
-   
-
-
-
-unittest.main()
